da14e4efb436b52e4bedbc9d424030738886fa30
Kirill Romanov <kirill_romanov2@epam.com>
2021-06-07 18:48:31 +0300

    feat: implement generator functions

--- app/10.sync-generators.js
+++ app/10.sync-generators.js
@@ -4,6 +4,13 @@ console.log('Topic: Sync-Generators');
 // RU: Наишите функцию-генератор generateSequence, которая генерирует числа в диапазоне от start до end.
 // EN: Create function-generator generateSequence, which generates and returns numbers from start to end.
 
+function* generateSequence(start, end) {
+  for (let i = start; i <= end; i++) {
+    yield i;
+  }
+}
+
+// console.log([...generateSequence(3, 15)]);
 
 // Task 2. Long Sequences
 // RU: Создать массив из чисел от 1 до 1000. 
@@ -14,11 +21,42 @@ console.log('Topic: Sync-Generators');
 //     Implement a generator function to create all pairs from this array.
 //     Result [1, 1], [1, 2], ..., [2, 1], [2, 2], ..., [1000, 1000]
 
+const sequence = []
+for (let i = 1; i <= 1000; i++) {
+  sequence.push(i);
+}
+
+function* longSequencePair(){
+  for (let i = 0; i < sequence.length; i++) {
+    for(let j = 0; j < sequence.length; j++) {
+      yield [sequence[i], sequence[j]];
+    }
+  }
+}
+
+// const it = longSequencePair();
+// console.log(it.next().value)
+// console.log(it.next().value)
+// console.log(it.next().value)
+// console.log(it.next().value)
 
 // Task 3. 
 // RU: Реализуйте функцию генерации чисел Фибоначчи до бесконечности
 // EN: Implement the infinite function-generator of generating Fibonacci numbers
 
+// let [prev, sum] = [0, 1];
+
+function* fibGenerator() {
+  const next = prev+ sum;
+  prev = sum;
+  sum = next;
+
+  yield sum;
+}
+
+// [...Array(10)].forEach(() => {
+//   console.log(fibGenerator().next().value)
+// })
 
 // Task 4. yield* 
 // RU: Реализуйте функцию-генератор, которая будет генерировать последовательность 
@@ -31,6 +69,20 @@ console.log('Topic: Sync-Generators');
 //     String.fromCharCode (code), and a range of numbers from 48 to 57,
 //     65 to 90 and 97 to 122
 
+function* symbolsSequence() {
+  // 0..9
+  yield* generateSequence(48, 57);
+  // A..Z
+  yield* generateSequence(65, 90);
+  // a..z
+  yield* generateSequence(97, 122);
+}
+
+let s = "";
+for (let code of symbolsSequence()) {
+  s += String.fromCharCode(code);
+}
+// console.log(s);
 
 // Task 5. Sync Piping
 // RU: Создайте-функцию генератор multipleByTwo, которая принимает массив чисел, 
@@ -56,6 +108,32 @@ console.log('Topic: Sync-Generators');
 //     Apply the spread operator to the result of the third function and create an array 
 //     of those numbers which will remain
 
+function* multipleByTwo(numbers) {
+  for (const number of numbers) {
+    yield number * 2;
+  }
+}
+
+function* minusOne(numbers) {
+  for (const number of numbers) {
+    yield number - 1;
+  }
+}
+
+function* isMultipleOfThree(numbers) {
+  for(const value of numbers) {
+    if (value % 3 === 0 && value) {
+      yield value;
+    }
+  }
+}
+
+const testArr = [0,1,2,3,4,5,6,7,8,9];
+
+// const res1 = multipleByTwo(testArr);
+// const res2 = [...minusOne(res1)];
+// const result = [...isMultipleOfThree(res2)];
+// console.log(result);
 
 // Task 6. Communication with Infinite Generator
 // RU: Реализуйте бесконечную функцию-генератор calculation, которая будет выводить выражения
@@ -66,6 +144,30 @@ console.log('Topic: Sync-Generators');
 //     a + b = ?, where a, b will be randomly generated and request a response from the user.
 //     If the answer is correct, then write true to the console, otherwise false.
 
+
+function* randomizer() {
+  while(1) {
+    yield Math.ceil(Math.random() * 100);
+  }
+}
+
+function* calculation() {
+  while(1) {
+    try {
+      let [a, b] = randomizer();
+      let result = a + b;
+      let guessedResult = parseInt(window.prompt(`${a} + ${b} = ?\nWhats the answer?`));
+      yield (result === guessedResult);
+    } catch (e) {
+      console.warn(`Failed calculating:\n${e}`)
+    }
+  }
+}
+
+// const it = calculation();
+// console.log(it.next().value);
+// it.throw("Test error");
+// console.log(it.next().value);
 
 // Task 7. throw method
 // RU: Модифицируйте код предыдущего задания так, чтобы при вызове it.throw() генератор 
@@ -86,11 +188,20 @@ console.log('Topic: Sync-Generators');
 //     Create a generator that receives this object as input
 //     and returns only admin names.
 
-// const users = {
-//   anna: false,
-//   boris: true, // admin
-//   christina: false,
-//   dave: false,
-//   elena: false,
-//   felix: true,  // admin
-// };
+const users = {
+  anna: false,
+  boris: true, // admin
+  christina: false,
+  dave: false,
+  elena: false,
+  felix: true,  // admin
+};
+
+function* yieldAdmins(users) {
+  const adminEntries = Object.entries(users).filter(([name, isAdmin]) => isAdmin).map((pair) => pair[0]);
+  yield* adminEntries;
+}
+
+// const itAdmins = yieldAdmins(users);
+// console.log(itAdmins.next().value);
+// console.log(itAdmins.next().value);
